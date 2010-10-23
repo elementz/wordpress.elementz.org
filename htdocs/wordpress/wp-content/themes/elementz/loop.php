3cc88a212a349d44522d79d6d838bf77314e6bd0
gerard sychay <hellogerard@gmail.com>
2010-10-23 18:57:31 -0400

    there was actually an author bio on single.php already. grab that. fix search results item background to match.

--- htdocs/wordpress/wp-content/themes/elementz/loop.php
+++ htdocs/wordpress/wp-content/themes/elementz/loop.php
@@ -160,10 +160,22 @@
 				<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="meta-sep">|</span> <span class="edit-link">', '</span>' ); ?>
 			</div><!-- .entry-utility -->
 
-			<div class="entry-bio">
-                <?php echo get_avatar(get_the_author_meta('user_email')); ?>
-				<em><?php twentyten_posted_on(); ?></em>. <?php echo get_the_author_meta('description'); ?>
-			</div><!-- .entry-bio -->
+<?php if ( get_the_author_meta( 'description' ) ) : // If a user has filled out their description, show a bio on their entries  ?>
+					<div id="entry-author-info">
+						<div id="author-avatar">
+							<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'twentyten_author_bio_avatar_size', 60 ) ); ?>
+						</div><!-- #author-avatar -->
+						<div id="author-description">
+							<h2><?php printf( esc_attr__( 'About %s', 'twentyten' ), get_the_author() ); ?></h2>
+							<?php the_author_meta( 'description' ); ?>
+							<div id="author-link">
+								<a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>">
+									<?php printf( __( 'View all posts by %s <span class="meta-nav">&rarr;</span>', 'twentyten' ), get_the_author() ); ?>
+								</a>
+							</div><!-- #author-link	-->
+						</div><!-- #author-description -->
+					</div><!-- #entry-author-info -->
+<?php endif; ?>
 		</div><!-- #post-## -->
 
 		<?php comments_template( '', true ); ?>
